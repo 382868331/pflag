52ae17b633346c2a26fe04b5fa13ef9e35ba9cdd
382868331 <382868331@users.noreply.github.com>
2026-08-24 00:07:53 +0800

    fix case 011: 范围端点顺序

--- pflag_ordered_bounds.go
+++ pflag_ordered_bounds.go
@@ -1,6 +1,8 @@
 package pflag
 
 func pflagOrderedBounds(a,b int) (min,max int) {
-	if a>b { return a,b }
+	if a>b {
+		return b,a
+	}
 	return a,b
 }
--- pflag_ordered_bounds_test.go
+++ pflag_ordered_bounds_test.go
@@ -9,3 +9,7 @@ func TestPflagOrderedBounds(t *testing.T) {
 	min,max:=pflagOrderedBounds(9,2)
 	if min!=2 || max!=9 { t.Fatalf("range=(%d,%d)",min,max) }
 }
+
+func TestPflagOrderedBoundsKeepsAscendingRange(t *testing.T) {
+	min,max:=pflagOrderedBounds(2,9); if min!=2 || max!=9 { t.Fatalf("range=(%d,%d)",min,max) }
+}
